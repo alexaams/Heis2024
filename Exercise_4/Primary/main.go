bc8f860cef12abe2c7586e642e97d3321fdf1268
Jørgen Hazeland Baugerud <jorgen.baugerud@moenmarin.no>
2024-02-20 14:16:48 +0100

    Restructured

--- Exercise_4/Primary/main.go
+++ Exercise_4/Primary/main.go
@@ -11,29 +11,49 @@ import (
 
 const address = "localhost:8080"
 
-var number int
+var ournumber int = 0
 
 func main() {
 
-	//Connect to UDP-socket
-	udpAddr, err := net.ResolveUDPAddr("udp", address)
-	if err != nil {
-		fmt.Println("Error resolving UDP address:", err)
-		return
-	}
+	//Creating a UDP-listener socket (not sure about the terminology)
+	conn := listenToUDP(address)
 
-	conn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		fmt.Println("Error listening on UDP port:", err)
-		return
+	//Initiating backup-phase with the listener-socket, data and specified deadline
+	backupPhase(&ournumber, 3, conn)
+	conn.Close()
+	//Start a new backup-process in a new terminal when no other is detected on UDP-broadcast
+	openTerminal()
+
+	//Creating a UDP-sender socket (not sure about the terminology)
+	conn_send := sendToUDP(address)
+
+	//Initiating primary-phase with the sender-socket, data and specified deadline
+	primaryPhase(&ournumber, 3, conn_send)
+
+}
+
+func primaryPhase(number *int, deadlineSecs int, conn *net.UDPConn) {
+	fmt.Println("---Initiating primary process---")
+	for {
+		_, err := conn.Write([]byte(fmt.Sprint(*number)))
+		fmt.Println(*number)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		*number++
+		time.Sleep(time.Second * 2)
 	}
+}
+
+func backupPhase(number *int, deadlineSecs int, conn *net.UDPConn) {
 	//Initiating backup-phase
 	fmt.Println("---Initiating backup process---")
 	buffer := make([]byte, 1024) //Creating buffer to receive data
-
 	for {
 
-		deadline := time.Now().Add(3 * time.Second)
+		deadline := time.Now().Add(time.Duration(deadlineSecs) * time.Second)
 		conn.SetReadDeadline(deadline)
 		n, addr, err := conn.ReadFromUDP(buffer)
 
@@ -48,46 +68,56 @@ func main() {
 			fmt.Println("Feil ved konvertering av streng til heltall:", err)
 			return
 		}
-		number = number_temp
-		fmt.Printf("Mottatt %d bytes fra %s: %d\n", n, addr, (number))
+		*number = number_temp
+		fmt.Printf("Mottatt %d bytes fra %s: %d\n", n, addr, (*number))
 	}
-	conn.Close()
-	//Start a new backup-process in a new terminal when no other is detected on UDP-broadcast
-	openTerminal()
-	//Switch over to primary process and start sending a number
-	conn_send, err := net.DialUDP("udp", nil, udpAddr)
+}
+
+func listenToUDP(address string) *net.UDPConn {
+	//Connect to UDP-socket
+	udpAddr, err := net.ResolveUDPAddr("udp", address)
 	if err != nil {
-		fmt.Println("Feil ved å åpne UDP-port: ", err)
+		fmt.Println("Error resolving UDP address: ", err)
+		return nil
 	}
-	fmt.Println("---Initiating primary process---")
-	for {
-		_, err = conn_send.Write([]byte(fmt.Sprint(number)))
-		fmt.Println(number)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		number++
-		time.Sleep(time.Second * 1)
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Error listening on UDP port:", err)
+		return nil
 	}
-	//Terminate terminal.
+	return conn
+}
 
+func sendToUDP(address string) *net.UDPConn {
+	udpAddr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		fmt.Println("Error resolving UDP address: ", err)
+		return nil
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		fmt.Println("Error opening UDP address: ", err)
+		return nil
+	}
+	return conn
 }
 
 func openTerminal() {
-	// Bestem kommandoen for å åpne en ny terminal og kjøre go run main.go
+	//Declaring pointer to Cmd
 	var cmd *exec.Cmd
 	switch runtime.GOOS {
 	case "windows":
-		// For Windows bruker vi "cmd.exe" for å åpne en ny terminal og kjøre go run main.go
+		//Executing terminal command which runs main.go in this folder
 		cmd = exec.Command("cmd.exe", "/c", "start", "cmd.exe", "/k", "go", "run", "main.go")
 	default:
+		//''Catching'' error
 		fmt.Println("Dette operativsystemet støttes ikke")
 		return
 	}
 
-	// Start den nye terminalen
+	//Start new terminal
 	err := cmd.Start()
 	if err != nil {
 		fmt.Println("Feil ved åpning av terminal: ", err)
