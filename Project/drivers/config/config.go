81d531ae65eee7ca54522f8c74df6472122afc7e
alexaams <alexaams@stud.ntnu.no>
2024-02-25 13:04:19 +0100

    config
    Her har du det jorgen(biatch)

---
+++ Project/drivers/config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"ProjectHeis/drivers/elevio"
+	"fmt"
+)
+
+// ---- Globals----
+const NumElevators int = 3
+const NumFloors int = 4
+const NumButtons int = 3
+
+
+var ElevatorID int = -1
+
+
+
+// ---------TYPES-----------
+type ReqList 			map[int]bool
+type AckList 			[NumElevators]bool
+type OrderList			[NumFloors]
+
+
+// ---------STRUCTS----------
+type Elevator struct {
+	Floor 				int
+	Direction 			elevio.MotorDirection
+	Requests			[NumFloors][NumElevators]bool
+	Status				int 	// 0:idle, 1:open, 2:moving, 3: obst
+
+}
+
+type PeersConnection struct{
+	Peers				[]string
+	New					[]string
+	Lost				[]string
+}
+
+type PeersData struct{
+	Elevator 			Elevator
+	Id					int
+	//Orders
+	//
+}
+
+
+
+
+
+
+
+
+// -------FUNCTIONS--------
+func MakeReqList(amountFloors, botFloor int) ReqList {
+	listFloor := make(map[int]bool)
+	for x := 0; x < amountFloors; x++ {
+		listFloor[x+botFloor] = false
+	}
+	return listFloor
+}
+
+func (r ReqList) SetFloor(floor int) {
+	if _, ok := r[floor]; ok {
+		r[floor] = true
+	} else {
+		fmt.Println("Floor does not exist")
+	}
+}
+
+func (r ReqList) ClearFloor(floor int) {
+	if _, ok := r[floor]; ok {
+		r[floor] = false
+	} else {
+		fmt.Println("Floor does not exist")
+	}
+}
