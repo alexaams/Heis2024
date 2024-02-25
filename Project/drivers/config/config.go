3a8bec9f969053fce38cef82054aa50ea9216b34
alexaams <alexaams@stud.ntnu.no>
2024-02-25 16:34:02 +0100

    config- gitignore - gomod

--- Project/drivers/config/config.go
+++ Project/drivers/config/config.go
@@ -10,45 +10,35 @@ const NumElevators int = 3
 const NumFloors int = 4
 const NumButtons int = 3
 
-
 var ElevatorID int = -1
 
-
-
 // ---------TYPES-----------
-type ReqList 			map[int]bool
-type AckList 			[NumElevators]bool
-type OrderList			[NumFloors]
-
+type ReqList map[int]bool
+type AckList [NumElevators]bool
+type OrdersCab [NumFloors][]bool
+type OrdersHall [][2]bool
 
 // ---------STRUCTS----------
 type Elevator struct {
-	Floor 				int
-	Direction 			elevio.MotorDirection
-	Requests			[NumFloors][NumElevators]bool
-	Status				int 	// 0:idle, 1:open, 2:moving, 3: obst
+	Floor     int
+	Direction elevio.MotorDirection
+	Requests  [NumFloors][NumElevators]bool
+	Behavior  int // 0:idle, 1:open, 2:moving, 3: obst
 
 }
 
-type PeersConnection struct{
-	Peers				[]string
-	New					[]string
-	Lost				[]string
+type PeersConnection struct {
+	Peers []string
+	New   []string
+	Lost  []string
 }
 
-type PeersData struct{
-	Elevator 			Elevator
-	Id					int
+type PeersData struct {
+	Elevator Elevator
+	Id       int
 	//Orders
 	//
 }
-
-
-
-
-
-
-
 
 // -------FUNCTIONS--------
 func MakeReqList(amountFloors, botFloor int) ReqList {
@@ -73,4 +63,19 @@ func (r ReqList) ClearFloor(floor int) {
 	} else {
 		fmt.Println("Floor does not exist")
 	}
+}
+
+func ElevatorBehaviorToString(elev Elevator) string {
+	behavior := elev.Behavior
+	switch behavior {
+	case 0:
+		return "idle"
+	case 1:
+		return "moving"
+	case 2:
+		return "obst"
+	default:
+		return "undefined"
+	}
+
 }
---
+++ Project/go.mod
@@ -0,0 +1,3 @@
+module ProjectHeis
+
+go 1.21.5
